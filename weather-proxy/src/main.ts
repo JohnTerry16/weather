fbcbef958a7139d1caade5ddf81c78f2643ba951
JohnTerry16 <semdashtun@gmail.com>
2024-09-30 16:13:41 +0300

    port fix

--- weather-proxy/src/main.ts
+++ weather-proxy/src/main.ts
@@ -8,7 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   console.log(`🚀 Backend is running on http://localhost:3000`);
 }
 bootstrap();
